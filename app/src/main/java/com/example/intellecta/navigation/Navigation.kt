0923bbdb5a1959f5bfc92cd7655aa96a8cd8420a
Mohammad Azam <mohdazam0311@gmail.com>
2025-08-29 21:51:38 +0530

    Navigation updated for NoteList and EditScreen

--- app/src/main/java/com/example/intellecta/navigation/Navigation.kt
+++ app/src/main/java/com/example/intellecta/navigation/Navigation.kt
@@ -5,10 +5,13 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.intellecta.ui.screens.AddNoteScreen
+import com.example.intellecta.ui.screens.EditNoteScreen
 import com.example.intellecta.ui.screens.HomeScreen
 import com.example.intellecta.ui.screens.NoteListScreen
 
@@ -44,7 +47,22 @@ fun Navigation(){
             },
             popEnterTransition = { slideInHorizontally(initialOffsetX = { -it }) + fadeIn() },
             popExitTransition = { slideOutHorizontally(targetOffsetX = { it }) + fadeOut() }){
-            NoteListScreen()
+            NoteListScreen(navCtrl)
+        }
+
+        composable(Screens.EditNoteScreen.route,
+            arguments = listOf(navArgument("noteId"){type = NavType.IntType}),
+            enterTransition = {
+                slideInHorizontally(initialOffsetX = { it }) + fadeIn()
+            },
+            exitTransition = {
+                slideOutHorizontally(targetOffsetX = { -it }) + fadeOut()
+            },
+            popEnterTransition = { slideInHorizontally(initialOffsetX = { -it }) + fadeIn() },
+            popExitTransition = { slideOutHorizontally(targetOffsetX = { it }) + fadeOut() }){
+                backStackEntry ->
+            val noteId = backStackEntry.arguments?.getInt("noteId") ?: 0
+            EditNoteScreen()
         }
     }
 }
--- app/src/main/java/com/example/intellecta/navigation/Screens.kt
+++ app/src/main/java/com/example/intellecta/navigation/Screens.kt
@@ -5,4 +5,7 @@ sealed class Screens(val route: String ) {
     data object HomeScreen      : Screens("home_screen")
     data object AddNoteScreen   : Screens("add_note_screen")
     data object NotesListScreen : Screens("notes_list_screen")
+    data object EditNoteScreen  : Screens("edit_note_screen/{noteId}"){
+        fun createRoute(noteId: Int) = "edit_note_screen/$noteId"
+    }
 }
