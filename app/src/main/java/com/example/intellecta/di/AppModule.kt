ad6f86def1d44c8468c7e7e896b6eaaf26b3f86e
Mohammad Azam <mohdazam0311@gmail.com>
2025-11-03 13:14:51 +0530

    Auth interceptor creation.

--- app/src/main/java/com/example/intellecta/di/AppModule.kt
+++ app/src/main/java/com/example/intellecta/di/AppModule.kt
@@ -5,13 +5,21 @@ import androidx.room.Room
 import com.example.intellecta.chatBot.ChatViewModel
 import com.example.intellecta.dao.IntellectaDatabase
 import com.example.intellecta.fileManaging.FileManager
+import com.example.intellecta.network.ApiService
+import com.example.intellecta.network.AuthInterceptor
 import com.example.intellecta.repository.FileStorageRepository
 import com.example.intellecta.repository.NoteRepository
 import com.example.intellecta.viewmodel.AuthViewModel
 import com.example.intellecta.viewmodel.FilesManagingViewModel
 import com.example.intellecta.viewmodel.NoteViewModel
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val appModule = module {
 
@@ -24,6 +32,33 @@ val appModule = module {
             "intellecta.db"
         ).build()
     }
+
+    //OkHttp Client with Auth Interceptor
+    single {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor(get())) // Inject TokenManager
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl("http://10.132.249.180:8080/") // Change to your backend URL
+            .client(get())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    single { get<Retrofit>().create(ApiService::class.java) }
+
+    //Token Manager
+    single { TokenManager(get()) }
 
     //DAOs
 
---
+++ app/src/main/java/com/example/intellecta/network/AuthInterceptor.kt
@@ -0,0 +1,27 @@
+package com.example.intellecta.network
+
+import com.example.intellecta.data.TokenManager
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(
+    private val tokenManager: TokenManager
+):Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        val token = tokenManager.getToken()
+        val request = if(token!=null){
+            originalRequest.newBuilder()
+                .addHeader("Authorization","Bearer $token")
+                .build()
+
+        }
+        else{
+            originalRequest
+        }
+
+        return chain.proceed(request)
+    }
+}
