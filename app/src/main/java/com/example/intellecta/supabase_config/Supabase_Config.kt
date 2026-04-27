37b65ed05b086f24c17c16a86da9055809e29303
Mohammad Azam <mohdazam0311@gmail.com>
2026-04-27 16:00:18 +0530

    Config : Supabase config

--- app/src/main/java/com/example/intellecta/supabase_config/Supabase_Config.kt
+++ app/src/main/java/com/example/intellecta/supabase_config/Supabase_Config.kt
@@ -1,18 +1,35 @@
 package com.example.intellecta.supabase_config
 
+import android.util.Log
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.functions.Functions
+import io.github.jan.supabase.gotrue.Auth
+import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.storage.Storage
 
 
 
 object SupabaseConfig {
+
+    private const val TAG = "SupabaseConfig"
+
     fun createClient(supabaseUrl: String, supabaseKey: String): SupabaseClient {
-        return createSupabaseClient(
-            supabaseUrl = supabaseUrl,
-            supabaseKey = supabaseKey
-        ) {
-            install(Storage)
+
+        return try {
+            createSupabaseClient(
+                supabaseUrl = supabaseUrl,
+                supabaseKey = supabaseKey
+            ) {
+                install(Storage)
+                install(Auth)
+                install(Postgrest)
+                install(Functions)
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "Error creating Supabase client: ${e.message}", e)
+            throw e
         }
     }
+
 }
